8a0f9fd9856adf454448877f4ec1e86cc6fa1f29
AndreasBroberg <Andreas.broberg096@hotmail.com>
2023-12-12 18:19:20 +0100

    Endnu mere lort
    Kæmpe lort

--- src/app/_components/boatDetails.tsx
+++ src/app/_components/boatDetails.tsx
@@ -1,32 +1,64 @@
-import React from "react";
-import { BoatData } from "~/types/mockBoats";
+import React, { useState } from "react";
+import type { BoatData } from "~/types/mockBoats";
 
 interface BoatDetailsProps {
   boats: BoatData[];
 }
 
+// Define the type for the log names
+type LogType =
+  | "bmsLogs"
+  | "chargerLogs"
+  | "coreMCULogs"
+  | "motorLogs"
+  | "throttleLogs";
+
 const BoatTable: React.FC<BoatDetailsProps> = ({ boats }) => {
+  const [selectedLogType, setSelectedLogType] = useState<LogType | null>(null);
+
   if (!boats.length) return <div>No boat data available.</div>;
   const boat = boats[0];
-  
-  // Make sure boat is defined before proceeding
-  if (!boat) return <div>The boat is undefined.</div>;
 
-  const renderLogDetails = (logArray?: any[], title?: string) => {
-    if (!logArray || logArray.length === 0) return <div>No {title} available.</div>;
+  // Function to render the log details for the selected log type
+  const renderLogDetails = (logType: LogType) => {
+    const logs = boat[logType];
+    if (!logs || logs.length === 0) return <div>No logs available.</div>;
+
+    // Display the first 10 logs or fewer if there aren't enough
+    const displayLogs = logs.slice(0, 10);
+
     return (
-      <div className="mb-4 p-2 border rounded shadow">
-        <h3 className="text-center bg-gray-100 text-gray-700 py-2 px-4 rounded-t font-medium text-lg">{title}</h3>
-        <table className="min-w-full bg-gray">
+      <div className="mb-4 w-full rounded border p-2 shadow">
+        <h3 className="rounded-t bg-gray-100 px-4 py-2 text-center text-lg font-medium text-gray-700">
+          {logType.charAt(0).toUpperCase() + logType.slice(1)}
+        </h3>
+        <table className="min-w-full">
+          <thead>
+            <tr className="border-b">
+              <th className="px-4 py-2">ID</th>
+              {/* Assuming all log entries have the same structure, so we can use the first one to get the headers */}
+              {Object.keys(displayLogs[0])
+                .slice(0, 5)
+                .map((key) => (
+                  <th key={key} className="px-4 py-2">
+                    {key}
+                  </th>
+                ))}
+            </tr>
+          </thead>
           <tbody>
-            {Object.entries(logArray[0])
-              .filter(([key]) => key !== 'timestamp')
-              .map(([key, value]) => (
-                <tr key={key} className="border-b">
-                  <td className="px-2 py-1 font-medium border border">{key}</td>
-                  <td className="px-2 py-1">{value as React.ReactNode}</td>
-                </tr>
-              ))}
+            {displayLogs.map((log, index) => (
+              <tr key={index} className="border-b">
+                <td className="px-4 py-2">{index + 1}</td>
+                {Object.entries(log)
+                  .slice(0, 5)
+                  .map(([key, value], valueIndex) => (
+                    <td key={valueIndex} className="px-4 py-2">
+                      {value.toString()}
+                    </td>
+                  ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -34,36 +66,33 @@ const BoatTable: React.FC<BoatDetailsProps> = ({ boats }) => {
   };
 
   return (
-    <div className="overflow-x-auto p-4">
-      <h1 className="text-2xl font-medium text-center uppercase mb-4">Boat Details</h1>
-      <div className="mb-4">
-        <div className="flex flex-wrap justify-center -mx-2">Boat ID: {boat.boatSystemId}</div>
-        <div className="flex flex-wrap justify-center -mx-2">Boat Name: {boat.boatSystemName}</div>
-        <div className="flex justify-center mt-8 space-x-4">
-        {/* Add your buttons here */}
-        <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-        BmsLogs
-        </button>
-        <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-        ChargerLogs
-        </button>
-        <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-        CoreMCULogs
-        </button>
-        <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-        MotorLogs
-        </button>
-        <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-        ThrottleLogs
-        </button>
+    <div className="w-full place-items-center overflow-x-auto p-4">
+      <h1 className="mb-4 text-center text-2xl font-medium uppercase">
+        Boat Details
+      </h1>
+      <div className="mb-4 flex justify-center space-x-4">
+        {/* Buttons for selecting the log type */}
+        {[
+          "bmsLogs",
+          "chargerLogs",
+          "coreMCULogs",
+          "motorLogs",
+          "throttleLogs",
+        ].map((logType) => (
+          <button
+            key={logType}
+            onClick={() => setSelectedLogType(logType)}
+            className={`rounded bg-gray-100 px-4 py-2 font-bold text-gray-700 hover:bg-gray-200 ${
+              selectedLogType === logType ? "bg-gray-300" : ""
+            }`}
+          >
+            {logType.charAt(0).toUpperCase() + logType.slice(1)}
+          </button>
+        ))}
       </div>
-      </div>
-      <div className="flex flex-wrap justify-center -mx-2">
-        {renderLogDetails(boat.bmsLogs, 'BMS Logs')}
-        {renderLogDetails(boat.chargerLogs, 'Charger Logs')}
-        {renderLogDetails(boat.coreMCULogs, 'Core MCU Logs')}
-        {renderLogDetails(boat.motorLogs, 'Motor Logs')}
-        {renderLogDetails(boat.throttleLogs, 'Throttle Logs')}
+      <div className="mx-auto flex w-[80%] flex-wrap justify-center">
+        {/* Render the details for the selected log type */}
+        {selectedLogType && renderLogDetails(selectedLogType)}
       </div>
     </div>
   );
